91746bb42e4ca5cac0ad6b7673bd47287f77521a
Alan Barbosa <alanzinhob@hotmail.com>
2024-03-31 16:42:25 -0300

    31_03

--- src/handler.js
+++ src/handler.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 module.exports.putData = async (event) => {
     try {
-        const client = new MongoClient('mongodb://localhost:27017');
+        const client = new MongoClient('mongodb://my-service-mongodb-1:27017');
         await client.connect();
 
         const db = client.db('mydb');
@@ -30,10 +30,35 @@ module.exports.putData = async (event) => {
 
 
 module.exports.getData = async (event) => {
-			return {
-					statusCode: 200,
-					body: JSON.stringify({ message: 'Olá Seja bem Vindo' })
-			};
+	try {
+    const { MongoClient } = require('mongodb');
+    const client = new MongoClient('mongodb://my-service-mongodb-1:27017');
+
+    await client.connect();
+
+    const db = client.db('mydb');
+    const collection = db.collection('mycollection');
+
+    // Executar o select
+    const result = await collection.find({}).toArray();
+
+    await client.close();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(result)
+    };
+} catch (error) {
+    console.error('Error querying data:', error);
+    return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'Internal server error' })
+    };
+}
+		// return {
+		// 		statusCode: 200,
+		// 		body: JSON.stringify({ message: 'Olá Seja bem Vindo Seu Puto' })
+		// 	};
 };
 
 
