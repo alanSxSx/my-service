34537b38e3de67f51edee6b032eacfbbd86f3ac9
Alan Barbosa <alanzinhob@hotmail.com>
2024-04-02 21:22:38 -0300

    02_04

--- src/handler.js
+++ src/handler.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 module.exports.putData = async (event) => {
     try {
-        const client = new MongoClient('mongodb://my-service-mongodb-1:27017');
+        const client = new MongoClient('mongodb://localhost:27017');
         await client.connect();
 
         const db = client.db('mydb');
@@ -13,11 +13,12 @@ module.exports.putData = async (event) => {
         const data = JSON.parse(event.body);
         await collection.insertOne(data);
 
+
         await client.close();
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'VEIO AQQUIIIIIII' })
+            body: JSON.stringify({ message: `Cadastro realizado com sucesso: ${event.body}` })
         };
     } catch (error) {
         console.error('Error inserting data:', error);
@@ -32,7 +33,7 @@ module.exports.putData = async (event) => {
 module.exports.getData = async (event) => {
 	try {
     const { MongoClient } = require('mongodb');
-    const client = new MongoClient('mongodb://my-service-mongodb-1:27017');
+    const client = new MongoClient('mongodb://localhost:27017');
 
     await client.connect();
 
@@ -63,9 +64,49 @@ module.exports.getData = async (event) => {
 
 
 module.exports.updateData = async (event) => {
-	return {
-			statusCode: 200,
-			body: JSON.stringify({ message: 'Alteração feita com Sucesso' })
-	};
+	try {
+		const client = new MongoClient('mongodb://localhost:27017');
+		await client.connect();
+
+		const db = client.db('mydb');
+		const collection = db.collection('mycollection');
+
+		const dataToUpdate = JSON.parse(event.body);
+
+		// Supondo que o ID do documento a ser atualizado seja passado no corpo da requisição
+		const { id } = dataToUpdate;
+
+		// Construir o filtro para encontrar o documento a ser atualizado
+		const filter = { _id: ObjectId(id) };
+
+		// Remover o id dos dados a serem atualizados
+		delete dataToUpdate.id;
+
+		// Construir o objeto de atualização
+		const updateData = { $set: dataToUpdate };
+
+		// Executar a operação de atualização
+		const result = await collection.updateOne(filter, updateData);
+
+		await client.close();
+
+		if (result.modifiedCount === 1) {
+				return {
+						statusCode: 200,
+						body: JSON.stringify({ message: 'Documento atualizado com sucesso' })
+				};
+		} else {
+				return {
+						statusCode: 404,
+						body: JSON.stringify({ message: 'Documento não encontrado' })
+				};
+		}
+} catch (error) {
+		console.error('Error updating data:', error);
+		return {
+				statusCode: 500,
+				body: JSON.stringify({ message: 'Internal server error' })
+		};
+}
 };
 
